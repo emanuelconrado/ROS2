0e87e80df6f8d516924fc0c30944ec24f6dccfc4
unknown <emanuelthadeu45@gmail.com>
2024-11-19 19:45:02 -0300

    Following turtle: 1.0

--- circle_draw/src/pose_subscriber.cpp
+++ circle_draw/src/pose_subscriber.cpp
@@ -1,57 +1,49 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "turtlesim/msg/pose.h"
+#include "turtlesim/msg/pose.hpp"
 #include <iostream>
 #include <string>
 #include <chrono>
 
-class draw_node : public rclcpp::Node
+class pose_node : public rclcpp::Node
 {
 
 private:
     int count_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr cmd_pos_sub;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
     geometry_msgs::msg::Twist msg;
 
-    void circle_draw(){
-
-        //anda 2 para frente
-        msg.linear.x = 2;
-        //roda em torno do eixo z
-        msg.angular.z = 1;
-        
-        RCLCPP_INFO(this->get_logger(), "Publishing: Position (%.2f, %.2f, %.2f)", msg.linear.x, msg.linear.y, msg.linear.z);
-
-        //publicando mensagem do node
-        cmd_vel_pub->publish(msg);
+    void pose_update(const turtlesim::msg::Pose::SharedPtr msg){
+        RCLCPP_INFO(this->get_logger(), "X: %.f Y: %.f\n", msg->linear_velocity, msg->angular_velocity);
     }
 
+    void circle_draw(const turtlesim::msg::Pose::SharedPtr msg){
 
+        //anda 2 para frente
+        this->msg.linear.x = msg->linear_velocity + 2;
+        //roda em torno do eixo z
+        this->msg.angular.z = msg->angular_velocity + 3;
+        
+        RCLCPP_INFO(this->get_logger(), "Publishing: Position (%.2f, %.2f, %.2f)", this->msg.linear.x, this->msg.linear.y, this->msg.linear.z);
 
-    void timer_callback(){
-        this->count_++;
-        RCLCPP_INFO(this->get_logger(), "Hello, World %d", this->count_);
+        //publicando mensagem do node
+        cmd_vel_pub->publish(this->msg);
     }
 
 public:
-    draw_node() : Node("circle_draw"){
-        RCLCPP_INFO(this->get_logger(), "Draw node started");
-
-        //criando topic de nome turtle1/cmd_vel com tamanho 10 de buffer
-        this->cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
-
-        //chama a função circle_draw a cada 0.1 segundos
-        timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&draw_node::circle_draw, this));
+    pose_node() : Node("pose_subscriber"){
+        this->cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 10);
+        this->cmd_pos_sub = this->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10, std::bind(&pose_node::circle_draw, this, std::placeholders::_1));
     }
 };
 
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<draw_node>();
+    auto node = std::make_shared<pose_node>();
     rclcpp::spin(node);
-
     rclcpp::shutdown();
     return 0;
 }
